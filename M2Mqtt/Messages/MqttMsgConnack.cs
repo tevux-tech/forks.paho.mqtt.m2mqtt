6908d8ba55fb1735578fcc610db3e16ee2c1487a
devbotas <simon@devbot.lt>
2021-09-14 17:45:41 +0300

    Making MqttMsgConnack lean. Removing code not needed for client side.

--- M2Mqtt/Messages/MqttMsgConnack.cs
+++ M2Mqtt/Messages/MqttMsgConnack.cs
@@ -33,21 +33,8 @@ namespace uPLibrary.Networking.M2Mqtt.Messages {
 
         }
 
-        private const byte TOPIC_NAME_COMP_RESP_BYTE_OFFSET = 0;
-        private const byte TOPIC_NAME_COMP_RESP_BYTE_SIZE = 1;
-        // [v3.1.1] connect acknowledge flags replace "old" topic name compression respone (not used in 3.1)
-        private const byte CONN_ACK_FLAGS_BYTE_OFFSET = 0;
-        private const byte CONN_ACK_FLAGS_BYTE_SIZE = 1;
-        // [v3.1.1] session present flag
-        private const byte SESSION_PRESENT_FLAG_MASK = 0x01;
-        private const byte SESSION_PRESENT_FLAG_OFFSET = 0x00;
-        private const byte SESSION_PRESENT_FLAG_SIZE = 0x01;
-        private const byte CONN_RETURN_CODE_BYTE_OFFSET = 1;
-        private const byte CONN_RETURN_CODE_BYTE_SIZE = 1;
-
-        // [v3.1.1] session present flag
         /// <summary>
-        /// Session present flag
+        /// Session present flag [v3.1.1]
         /// </summary>
         public bool SessionPresent { get; set; }
 
@@ -58,6 +45,13 @@ namespace uPLibrary.Networking.M2Mqtt.Messages {
         }
 
         public static MqttMsgConnack Parse(byte fixedHeaderFirstByte, byte protocolVersion, IMqttNetworkChannel channel) {
+            // Section 3.2.2.
+            // Byte 1: ConAck flags.
+            // Byte 2: Return code.
+            byte flagsByteOffset = 0; // <-- [v3.1.1] connect acknowledge flags replace "old" topic name compression respone (not used in 3.1)
+            byte flagsbyteMask = 0x01;// <-- [v3.1.1] session present flag
+            byte returnCodeByteOffset = 1;
+
             byte[] buffer;
             var msg = new MqttMsgConnack();
 
@@ -76,71 +70,17 @@ namespace uPLibrary.Networking.M2Mqtt.Messages {
             channel.Receive(buffer);
             if (protocolVersion == MqttMsgConnect.PROTOCOL_VERSION_V3_1_1) {
                 // [v3.1.1] ... set session present flag ...
-                msg.SessionPresent = (buffer[CONN_ACK_FLAGS_BYTE_OFFSET] & SESSION_PRESENT_FLAG_MASK) != 0x00;
+                msg.SessionPresent = (buffer[flagsByteOffset] & flagsbyteMask) != 0x00;
             }
             // ...and set return code from broker
-            msg.ReturnCode = buffer[CONN_RETURN_CODE_BYTE_OFFSET];
+            msg.ReturnCode = buffer[returnCodeByteOffset];
 
             return msg;
         }
 
         public override byte[] GetBytes(byte ProtocolVersion) {
-            var fixedHeaderSize = 0;
-            var varHeaderSize = 0;
-            var payloadSize = 0;
-            var remainingLength = 0;
-            byte[] buffer;
-            var index = 0;
-
-            if (ProtocolVersion == MqttMsgConnect.PROTOCOL_VERSION_V3_1_1) {
-                // flags byte and connect return code
-                varHeaderSize += (CONN_ACK_FLAGS_BYTE_SIZE + CONN_RETURN_CODE_BYTE_SIZE);
-            }
-            else {
-                // topic name compression response and connect return code
-                varHeaderSize += (TOPIC_NAME_COMP_RESP_BYTE_SIZE + CONN_RETURN_CODE_BYTE_SIZE);
-            }
-
-            remainingLength += (varHeaderSize + payloadSize);
-
-            // first byte of fixed header
-            fixedHeaderSize = 1;
-
-            var temp = remainingLength;
-            // increase fixed header size based on remaining length
-            // (each remaining length byte can encode until 128)
-            do {
-                fixedHeaderSize++;
-                temp = temp / 128;
-            } while (temp > 0);
-
-            // allocate buffer for message
-            buffer = new byte[fixedHeaderSize + varHeaderSize + payloadSize];
-
-            // first fixed header byte
-            if (ProtocolVersion == MqttMsgConnect.PROTOCOL_VERSION_V3_1_1) {
-                buffer[index++] = (MessageType.ConAck << FixedHeader.TypeOffset) | MessageFlags.ConAck; // [v.3.1.1]
-            }
-            else {
-                buffer[index++] = (byte)(MessageType.ConAck << FixedHeader.TypeOffset);
-            }
-
-            // encode remaining length
-            index = EncodeRemainingLength(remainingLength, buffer, index);
-
-            if (ProtocolVersion == MqttMsgConnect.PROTOCOL_VERSION_V3_1_1) {
-                // [v3.1.1] session present flag
-                buffer[index++] = SessionPresent ? (byte)(1 << SESSION_PRESENT_FLAG_OFFSET) : (byte)0x00;
-            }
-            else {
-                // topic name compression response (reserved values. not used);
-                buffer[index++] = 0x00;
-            }
-
-            // connect return code
-            buffer[index++] = ReturnCode;
-
-            return buffer;
+            // Not needed for the client side.
+            return new byte[0];
         }
 
         public override string ToString() {
